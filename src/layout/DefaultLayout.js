048cc2138a4491644e1a87034fc1cf8e30f68f84
frankiiize <frankiiize@gmail.com>
2022-05-13 14:20:23 -0400

    feat: fileStructure & basic routing
    -file structure proposal -routin using react-router version 6 -added  private routes component & public routes structure

---
+++ src/layout/DefaultLayout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const DefaultLayout = () => {
+  return (
+    <nav>
+      <ul>
+        <li>link1</li>
+        <li>link2</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default DefaultLayout; 
