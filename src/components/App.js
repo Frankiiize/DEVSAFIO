d6b9194aea109803ed8e4adca2a2443a0261ac17
patoxo <patricio.cr95@protonmail.com>
2022-05-31 15:29:27 -0400

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { Route, Routes } from 'react-router-dom';
 import { ProtectedRoute } from '../routers/ProtectedRoute';
 import { Suspense } from 'react';
@@ -11,7 +12,11 @@ const IndexPage = React.lazy(() => import('../views/Index/IndexPage'));
 
 function App() {
   return (
-    <Suspense fallback={<h1>Loading...</h1>}>
+    <Suspense fallback={ <div class="flex items-center justify-center min-h-screen bg-primary">
+    <div class="spinner-border animate-spin inline-block w-24 h-24 border-4 rounded-full text-white" role="status">
+      <span class="visually-hidden">Loading...</span>
+    </div>
+  </div>}>
       <Routes>
         <Route path='/' element={<IndexPage/>} />
         <Route path='/login' element={<h1>Login</h1>} />
