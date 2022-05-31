e2449044607f8eb3a36d3d1f8be8306809e0f3b0
frankiiize <frankiiize@gmail.com>
2022-05-31 19:26:30 -0400

    hotfix: fixed name class to className  on suspense component

--- src/components/App.js
+++ src/components/App.js
@@ -13,9 +13,9 @@ const Login = React.lazy(() => import('../views/Login/Login'));
 
 function App() {
   return (
-    <Suspense fallback={ <div class="flex items-center justify-center min-h-screen bg-primary">
-    <div class="spinner-border animate-spin inline-block w-24 h-24 border-4 rounded-full text-white" role="status">
-      <span class="visually-hidden">Loading...</span>
+    <Suspense fallback={ <div className="flex items-center justify-center min-h-screen bg-primary">
+    <div className="spinner-border animate-spin inline-block w-24 h-24 border-4 rounded-full text-white" role="status">
+      <span className="visually-hidden">Loading...</span>
     </div>
   </div>}>
       <Routes>
