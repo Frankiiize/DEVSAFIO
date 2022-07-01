d5fd7b11d31c3dccbb7bc04ce3bef03a8ce35ec2
frankiiize <frankiiize@gmail.com>
2022-07-01 17:05:04 -0400

    testing steps

--- src/views/postulate/Postulate.js
+++ src/views/postulate/Postulate.js
@@ -2,145 +2,16 @@ import { useFormik } from "formik";
 import React, { useEffect, useState } from "react";
 import { InputComponent } from "../../components/common/InputComponent";
 import { jopProfileShema as schema } from "../../components/schemas/schema";
-import { validate, handlerInputChangeCreator } from "../../utils/utils";
+import { validate, handlerInputChangeCreator, getErrorsFromValidationError } from "../../utils/utils";
 import { ButtonComponent } from "../../components/common/ButtonComponent";
 import { FormsCardContainer } from "../../layout/FormsCardContainer";
-
-const inputConfig = [
-  {
-    type: "text",
-    id: "name",
-    placeholder: "tu nombre",
-    name: "name",
-    label: "nombre",
-    autoComplete: "given-name",
-  },
-  {
-    type: "text",
-    id: "lastName",
-    placeholder: "tu apellido",
-    name: "lastName",
-    label: "Apellido",
-    autoComplete: "family-name",
-  },
-  {
-    type: "email",
-    id: "email",
-    placeholder: "email@gmail.com",
-    name: "email",
-    label: "email",
-    autoComplete: "email",
-  },
-  {
-    type: "tel",
-    id: "number",
-    placeholder: "numero telefono",
-    name: "number",
-    label: "numero telefono",
-    autoComplete: "number",
-  },
-  {
-    type: "text",
-    id: "city",
-    placeholder: "donde vives",
-    name: "city",
-    label: "¿donde vives?",
-    autoComplete: "",
-  },
-  {
-    type: "text",
-    id: "country",
-    placeholder: "pais",
-    name: "country",
-    label: "¿pais de residencia?",
-    autoComplete: "",
-  },
-  {
-    titleHead: {
-      show: true,
-      title: "Cargo al que optas:",
-    },
-    type: "checkbox",
-    id: "DevFrontEnd",
-    name: "DevFrontEnd",
-    label: "Desarrollador Front End",
-  },
-  {
-    type: "checkbox",
-    id: "DevBackend",
-    name: "DevBackend",
-    label: "Desarrollador Backend",
-  },
-  {
-    type: "checkbox",
-    id: "DevFullStackBackend",
-    name: "DevFullStackBackend",
-    label: "Desarrollador Full Stack",
-  },
-  {
-    type: "checkbox",
-    id: "DesignerUXUI",
-    name: "DesignerUXUI",
-    label: "Diseñador UX/UI Research",
-  },
-  {
-    type: "checkbox",
-    id: "DesignerUI",
-    name: "DesignerUI",
-    label: "Diseñador UI",
-  },
-  {
-    type: "checkbox",
-    id: "AnalystQA",
-    name: "AnalystQA",
-    label: "QA Tester",
-  },
-  {
-    type: "checkbox",
-    id: "DevMobile",
-    name: "DevMobile",
-    label: "Desarrollador Mobile",
-  },
-  {
-    type: "checkbox",
-    id: "DataAnalist",
-    name: "DataAnalist",
-    label: "Administrador de Base de Datos",
-  },
-  {
-    type: "checkbox",
-    id: "DataScientist",
-    name: "DataScientist",
-    label: "Data Scientist o Especialista Machine Learning",
-  },
-  {
-    type: "checkbox",
-    id: "ProductManager",
-    name: "ProductManager",
-    label: "Product Manager",
-  },
-  {
-    type: "checkbox",
-    id: "DevOps",
-    name: "DevOps",
-    label: "DevOps o SRE",
-  },
-  {
-    type: "checkbox",
-    id: "EnginerData",
-    name: "EnginerData",
-    label: "Ingeniero de Datos",
-  },
-  {
-    type: "checkbox",
-    id: "Other",
-    name: "Other",
-    label: "Otra",
-  },
-];
+//inputsConfig
+import { stepOneInputs } from "./data/inputsConfig";
 
 const JobProfile = () => {
+  const [formSteps, setFormSteps] = useState(1);
   const [check, setCheck] = useState(false);
+  const [ triggeringValidation, setTriggeringValidation ] = useState(false);
 
   const onSubmit = (values) => {
     if (
@@ -188,9 +59,9 @@ const JobProfile = () => {
     },
     onSubmit: onSubmit,
     validate: validate(schema),
+    
   });
   const handleTxtChange = handlerInputChangeCreator(formik);
-  // console.log(formik)
   useEffect(() => {
     setCheck(false);
   }, [
@@ -216,79 +87,105 @@ const JobProfile = () => {
         <section className=" flex grow items-center">
           <div className="container mb-10  mx-auto mt-4">
             <FormsCardContainer>
-              <div className="px-5">
-                <h1 className="text-3xl ">Únete a Devsafío</h1>
-                <h4 className="text-m  pb-5"> Formulario de Inscripción</h4>
+              {
+                formSteps === 1 && (
+                  <div className="px-5">
+                    <h1 className="text-3xl ">Únete a Devsafío</h1>
+                    <h4 className="text-m  pb-5"> Formulario de Inscripción</h4>
 
-                <p className="mt-5">
-                  Devsafio es una iniciativa que une talento junior
-                  lationamericano(trainee y bootcamp) junto a mentores para
-                  potenciar su empleabilidad inicial e impactar la industria
-                  tecnológica.
-                </p>
-                <p className="mt-2 ">
-                  Si estás{" "}
-                  <span className="font-bold">
-                    {" "}
-                    buscando empleo Junior o Trainee (Programación, Diseño UX/UI
-                    y Data Science)
-                  </span>{" "}
-                  te dejamos este formulario para que seas parte de distintas
-                  ofertas laborales que tenemos junto a importantes empresas en
-                  latinoamerica.
-                </p>
-                <p className="mt-2 ">
-                  Recuerda que todos los{" "}
-                  <span className="font-bold">
-                    {" "}
-                    talentos juniors contratados son apoyados y guiados por un
-                    mentor senior durante 3 meses{" "}
-                  </span>
-                  (ofertas remuneradas).
-                </p>
-                <p className="mt-2 ">
-                  {" "}
-                  Nos puedes seguir en:{" "}
-                  <a
-                    className="text-blue-600"
-                    href={"https://www.linkedin.com/company/devsafio"}
-                  >
-                    Linkedin
-                  </a>
-                </p>
+                    <p className="mt-5">
+                      Devsafio es una iniciativa que une talento junior
+                      lationamericano(trainee y bootcamp) junto a mentores para
+                      potenciar su empleabilidad inicial e impactar la industria
+                      tecnológica.
+                    </p>
+                    <p className="mt-2 ">
+                      Si estás{" "}
+                      <span className="font-bold">
+                        {" "}
+                        buscando empleo Junior o Trainee (Programación, Diseño UX/UI
+                        y Data Science)
+                      </span>{" "}
+                      te dejamos este formulario para que seas parte de distintas
+                      ofertas laborales que tenemos junto a importantes empresas en
+                      latinoamerica.
+                    </p>
+                    <p className="mt-2 ">
+                      Recuerda que todos los{" "}
+                      <span className="font-bold">
+                        {" "}
+                        talentos juniors contratados son apoyados y guiados por un
+                        mentor senior durante 3 meses{" "}
+                      </span>
+                      (ofertas remuneradas).
+                    </p>
+                    <p className="mt-2 ">
+                      {" "}
+                      Nos puedes seguir en:{" "}
+                      <a
+                        className="text-blue-600"
+                        href={"https://www.linkedin.com/company/devsafio"}
+                      >
+                        Linkedin
+                      </a>
+                    </p>
 
-                <p className="pt-4 ">¡Vamos con todo!</p>
-                <h1 className="text-2xl  pt-4"> Información Personal</h1>
-              </div>
+                    <p className="pt-4 ">¡Vamos con todo!</p>
+                    <h1 className="text-2xl  pt-4"> Información Personal</h1>
+                  </div>
+                )
+              }
               <form className="p-1" onSubmit={formik.handleSubmit}>
                 <div className="p-4">
-                  {inputConfig.map((item, index) => (
-                    <InputComponent
-                      key={`input-${index}${item.name}`}
-                      label={item.label}
-                      type={item.type}
-                      titleHead={item.titleHead}
-                      id={item.id}
-                      placeholder={item.placeholder}
-                      name={item.name}
-                      formik={formik}
-                      onChange={handleTxtChange}
-                    />
-                  ))}
-
-                  {check === true ? (
-                    <span className="text-error">
-                      Seleccionar al menos una casilla
-                    </span>
-                  ) : (
-                    ""
+                 
+                  {formSteps === 1 && (
+            
+                    <>
+                      {
+                      stepOneInputs.map((item, index) => (
+                        <InputComponent
+                          key={`input-${index}${item.name}`}
+                          label={item.label}
+                          type={item.type}
+                          titleHead={item.titleHead}
+                          id={item.id}
+                          placeholder={item.placeholder}
+                          name={item.name}
+                          formik={formik}
+                          onChange={handleTxtChange}
+                        />
+                      ))
+                      }
+                      {check === true ? (
+                        <span className="text-error">
+                          Seleccionar al menos una casilla
+                        </span>
+                      ) : (
+                        ""
+                      )}
+                    </>
                   )}
                 </div>
-
+                {
+                    triggeringValidation && (
+                      <span className="text-error">llena el formulario</span>
+                    )
+                  }
                 <ButtonComponent
                   style={"primary"}
-                  type={"submit"}
+                  type={"button"}
                   label={"siguiente"}
+                  onClick={ () => formik.validateForm().then((res) =>{
+                    debugger
+                    if(Object.entries(res).length > 0){
+                      console.log(res)
+                      setTriggeringValidation(true)
+                    } else {
+                      console.log(formik)
+                      setTriggeringValidation(false)
+                      setFormSteps(() => formSteps + 1)
+                    }
+                  })}
                 ></ButtonComponent>
               </form>
             </FormsCardContainer>
