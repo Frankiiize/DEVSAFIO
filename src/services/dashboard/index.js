054b0b1c95b5a5b3e85fc53321b1aeeadab1058f
vverac <viverace@gmail.com>
2022-08-18 17:21:45 -0400

    commit : create jobs getonboard

---
+++ src/services/dashboard/index.js
@@ -0,0 +1,8 @@
+import request from "../index";
+
+export const register = async function (user) {
+  return request({
+    method: "GET",
+    url: `URL desafio latam`,
+  });
+};
--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -1,34 +1,36 @@
 import React, { useContext, useEffect, useState } from "react";
 import { authContext } from "../context/authContext";
+import JobBoardLayout from "../layout/JobBoardLayout";
 
 const Wellcome = (user) => {
-    return (
-      <div className="alert alert-info shadow-lg">
-          <div>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-            <span>bienvenido {user.user.name}{user.user.lastname}</span>
-          </div>
-        </div>
-    )
+  return (
+    <div className="alert alert-info shadow-lg">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-current flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+        <span>bienvenido {user.user.name}{user.user.lastname}</span>
+      </div>
+    </div>
+  )
 }
 
 const Dashboard = () => {
   const { user } = useContext(authContext);
-  const [ showWellcome, setShowWellcome]  = useState(true)
+  const [showWellcome, setShowWellcome] = useState(true)
   useEffect(() => {
     setTimeout(() => {
       setShowWellcome(false);
-    },3000)
-  },[])
+    }, 3000)
+  }, [])
   return (
     <>
       {
-       (showWellcome)  && 
-         Wellcome(user)
+        (showWellcome) &&
+        Wellcome(user)
       }
-    <h1>bienvenido</h1>
+      <h1>bienvenido</h1>
+      <JobBoardLayout />
     </>
-    );
+  );
 };
 
 export default Dashboard;
